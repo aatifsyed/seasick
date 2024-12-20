48ed3d3d91eb518f3e0b4788d91fdf59ef8ec4ee
Aatif Syed <aatifsyedyp@gmail.com>
2024-12-20 17:30:43 +0000

    feat: nul_terminated niches, Default, From

--- src/nul_terminated.rs
+++ src/nul_terminated.rs
@@ -1,4 +1,38 @@
-//! A common idiom in C is to provide a nul-terminated array of pointers.
+//! Iterators for nul-terminated arrays of pointers.
+//!
+//! A common C idiom is to have interfaces like this
+//! ```c
+//! /** count the number of strings in nul-terminated `arr`. */
+//! unsigned int count(char *arr[]);
+//!
+//! char *arr[] = { "hello", "world", NULL };
+//!                                // ^^^^
+//! assert(count(arr) == 2);
+//! ```
+//!
+//! Which translates to the following.
+//!
+//! ```
+//! # use seasick::*;
+//! # use std::ffi::*;
+//! mod bindings {
+//!     # use std::ffi::*;
+//!     extern "C" { pub fn count(arr: *mut *mut c_char) -> c_uint; }
+//! }
+//!
+//! #[no_mangle]
+//! extern "C" fn count(arr: Option<nul_terminated::Iter<SeaStr>>) -> c_uint {
+//!     arr.unwrap_or_default().count() as _
+//! }
+//!
+//! assert_abi! { fn count = bindings::count as unsafe extern "C" fn(_) -> _ }
+//!
+//! ```
+//!
+//! Note that the niche optimization for [`Option`] is guaranteed,
+//! and empty [`Default`] iterators are provided.
+//!
+//! # Layout
 //!
 //! ```text
 //!   ...   ...
@@ -6,18 +40,15 @@
 //! │ *T₀ │ *T₁ │ nul │
 //! └─────┴─────┴─────┘
 //!    ↑
-//!   base
+//!   base, an aligned pointer
 //! ```
-//!
-//! The module provides iterators for these arrays.
-//!
-//! Note that the pointers MUST be aligned.
 
-use core::{fmt, marker::PhantomData, ops::RangeFrom, ptr::NonNull};
+use core::{fmt, marker::PhantomData, ptr::NonNull};
 
 /// Iterator of `&T`.
 ///
 /// See [module documentation](mod@self) for more.
+#[repr(transparent)]
 pub struct Iter<'a, T> {
     raw: Raw<T>,
     life: PhantomData<&'a T>,
@@ -25,12 +56,22 @@ pub struct Iter<'a, T> {
 
 impl<T> Iter<'_, T> {
     /// # Safety
-    /// - Must have the layout described in [module documentation](mod@self).
-    /// - Pointers must be valid for reads.
-    /// - Objects must be valid for the given lifetime.
+    /// - `base` must be valid for [reads](std::ptr::read) (aligned, initialized, etc).
+    /// - `base` must point to a nul-terminated array of pointers,
+    ///   as described in the [module documentation](mod@self).
+    /// - Iterated objects must not be written to for the provided lifetime.
     pub unsafe fn new(base: NonNull<*const T>) -> Self {
         Self {
             raw: Raw::new(base.cast()),
+            life: PhantomData,
+        }
+    }
+}
+
+impl<T> Default for Iter<'_, T> {
+    fn default() -> Self {
+        Self {
+            raw: Raw::default(),
             life: PhantomData,
         }
     }
@@ -62,6 +103,7 @@ impl<T: fmt::Debug> fmt::Debug for Iter<'_, T> {
 /// Iterator of `&mut T`.
 ///
 /// See [module documentation](mod@self) for more.
+#[repr(transparent)]
 pub struct IterMut<'a, T> {
     raw: Raw<T>,
     life: PhantomData<&'a mut T>,
@@ -69,13 +111,22 @@ pub struct IterMut<'a, T> {
 
 impl<T> IterMut<'_, T> {
     /// # Safety
-    /// - Must have the layout described in [module documentation](mod@self).
-    /// - Pointers must be valid for writes.
-    /// - Objects must be valid for the given lifetime.
-    /// - Objects must not be written by other threads during the given lifetime.
+    /// - `base` must be valid for [reads](std::ptr::read) (aligned, initialized, etc).
+    /// - `base` must point to a nul-terminated array of pointers,
+    ///   as described in the [module documentation](mod@self).
+    /// - Must have exclusive access to the iterated objects for the provided lifetime.
     pub unsafe fn new(base: NonNull<*mut T>) -> Self {
         Self {
             raw: Raw::new(base),
+            life: PhantomData,
+        }
+    }
+}
+
+impl<T> Default for IterMut<'_, T> {
+    fn default() -> Self {
+        Self {
+            raw: Raw::default(),
             life: PhantomData,
         }
     }
@@ -111,36 +162,46 @@ impl<'a, T> From<IterMut<'a, T>> for Iter<'a, T> {
 /// Iterator of [`NonNull<T>`].
 ///
 /// See [module documentation](mod@self) for more.
+#[repr(transparent)]
 pub struct Raw<T> {
-    base: Option<NonNull<Option<NonNull<T>>>>,
-    off: RangeFrom<usize>,
+    ptr: NonNull<Option<NonNull<T>>>,
 }
 
 impl<T> Raw<T> {
     /// # Safety
-    /// - Must have the layout described in [module documentation](mod@self).
+    /// - `base` must be valid for [reads](std::ptr::read) (aligned, initialized, etc).
+    /// - `base` must point to a nul-terminated array of pointers,
+    ///   as described in the [module documentation](mod@self).
     pub unsafe fn new(base: NonNull<*mut T>) -> Self {
-        Self {
-            base: Some(base.cast()),
-            off: 0..,
-        }
+        assert!(base.is_aligned());
+        Self { ptr: base.cast() }
+    }
+}
+
+impl<T> Iterator for Raw<T> {
+    type Item = NonNull<T>;
+    fn next(&mut self) -> Option<Self::Item> {
+        let here = unsafe { self.ptr.read() }?;
+        unsafe { self.ptr = self.ptr.add(1) }
+        Some(here)
     }
 }
 
 impl<T> Clone for Raw<T> {
     fn clone(&self) -> Self {
-        Self {
-            base: self.base,
-            off: self.off.clone(),
-        }
+        Self { ptr: self.ptr }
     }
 }
+
 impl<T> Default for Raw<T> {
+    /// Empty by default.
     fn default() -> Self {
-        Self {
-            base: None,
-            off: 0..,
+        trait Empty {
+            const ARR: [Option<NonNull<Self>>; 1] = [None];
         }
+        impl<T: ?Sized> Empty for T {}
+        let base = NonNull::<Option<NonNull<Self>>>::from(&Self::ARR[0]);
+        unsafe { Raw::new(base.cast()) }
     }
 }
 impl<T> fmt::Debug for Raw<T> {
@@ -151,23 +212,15 @@ impl<T> fmt::Debug for Raw<T> {
     }
 }
 
-impl<T> Iterator for Raw<T> {
-    type Item = NonNull<T>;
-    fn next(&mut self) -> Option<Self::Item> {
-        let Self { base, off } = self;
-        unsafe {
-            let nxt = base.as_mut()?.add(off.next()?);
-            match nxt.as_ref() {
-                Some(it) => {
-                    debug_assert!(it.is_aligned());
-                    Some(*it)
-                }
-                None => {
-                    *base = None;
-                    None
-                }
-            }
-        }
+impl<T> From<Iter<'_, T>> for Raw<T> {
+    fn from(value: Iter<T>) -> Self {
+        value.raw
+    }
+}
+
+impl<T> From<IterMut<'_, T>> for Raw<T> {
+    fn from(value: IterMut<T>) -> Self {
+        value.raw
     }
 }
 
@@ -179,4 +232,18 @@ where
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_list().entries(self.0.clone()).finish()
     }
+}
+
+#[test]
+fn test() {
+    type T = u8;
+    const LEN: usize = 2;
+
+    let arr: [*mut T; LEN + 1] = core::array::from_fn(|ix| match ix == LEN {
+        true => 0 as _,
+        false => 1 as _,
+    });
+    let ct = unsafe { Raw::<T>::new(NonNull::from(&arr[0])) }.count();
+    assert_eq!(ct, LEN);
+    assert_eq!(Raw::<T>::default().count(), 0);
 }
